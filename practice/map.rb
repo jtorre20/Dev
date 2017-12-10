94338c84164581a687f9550c1080fe26e8149dd3
jtorre20 <jtorres1989@gmail.com>
2017-12-09 22:05:07 -0500

    new files

--- practice/map.rb
+++ practice/map.rb
@@ -1,5 +1,34 @@
+=begin   
 arr = [1,2,3,4,5]
 brr = arr.map{|x| -x}
-
+puts arr
+puts brr
 puts arr.join(", ")
 puts brr.join(", ")
+=end
+require "pry"
+
+toppings = ["pickles", "mushrooms", "bacon"]
+=begin
+def hamburger(toppings)
+    my_statements = []
+
+    toppings.each do |topping|
+      my_statements << "I love #{topping} on my burgers!"
+binding.pry # For some reason it is not stopping at this binding.pry
+    end
+binding.pry    
+    my_statements  
+end
+binding.pry
+puts "hello"
+=end
+
+def burger(toppings)
+    toppings.collect do |topping|
+      puts "I love #{topping} on my burgers"
+binding.pry # ignores binding.pry
+    end
+end
+
+  
